8ab6ab9f79cdb7bbc2383f987bfe94a4167469a1
sei <sei.email@example.com>
2023-01-26 11:05:49 +0000

    3.4.2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,29 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  # 各テストが実行される直前に実行される特別なメソッド
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     # getするurlはヘルパーメソッドを書く
     get static_pages_about_url
+    # サーバーからのｈｔｔｐステータスコードが２００かどうかを調べる。
     assert_response :success
+    # 表示されたページの中に特定のhtmlタグがあるかを調べる。
+    assert_select "title", "About | #{@base_title}"
   end
 end
