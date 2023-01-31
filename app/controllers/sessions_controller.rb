961414f6479800ad18e6543bd228548b191814b5
sei <sei.email@example.com>
2023-01-31 21:30:00 +0000

    10.3.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url] #直前にアクセスしようとしてはじかれたURLを取得しておく。
       reset_session #ログインの直前に必ずこれを書くこと
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
-      redirect_to user
+      redirect_to forwarding_url || user #直前のアクセスURLがあればそこにリダイレクトなければ、プロフィールへリダイレクト
     else
       # エラーメッセージを作成する。
       flash.now[:danger] = 'Invalid email/password combination'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,11 +4,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  # ログインしてないユーザーがindexアクションをリクエストしたときにログイン画面がリダイレクトされるかのテスト
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 
   # editとupdateアクションの保護に対するテスト
@@ -23,5 +30,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
+  end
+
+  # 間違ったユーザーが編集しようとしたときのテスト
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
