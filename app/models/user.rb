09f1d157b70ccac5169bcc5c05d4b781ec91088f
sei <sei.email@example.com>
2023-01-31 10:06:36 +0000

    9.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # 記憶トークンに対応する仮属性を用意する。
+  attr_accessor :remember_token
+
   # コールバック
   before_save { email.downcase! }
 
@@ -14,12 +17,34 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   # 渡された文字列のハッシュ値を返す。クラスメソッド
-  def User.digest(string)
+  def self.digest(string)
     # コストパラメータの設定。高いほど、オリジナルパスワードを計算で推測することが困難
     # テストの場合は、コストを最小にし、本番環境では高いコストで計算する
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     # fixture用のパスワードを生成
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token #記憶トークンを作成
+    update_attribute(:remember_digest, User.digest(remember_token)) #記憶ダイジェストを更新　なんでここはselfいらないの？
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
