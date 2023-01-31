efb8fffdd2732d8e61b79f3c769e11f66ad810c0
sei <sei.email@example.com>
2023-01-31 23:32:42 +0000

    10.4.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     # indexアクションでUsersをページネートする
@@ -43,6 +44,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path, status: :see_other
+  end
+
+
+
   private
     # ストロングパラメータ
     def user_params
@@ -65,5 +74,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+
+    # ログインユーザの権限が管理者でない場合、ルートへリダイレクトする
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
 end
