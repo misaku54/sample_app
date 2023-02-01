ea1d6a74546b54d0bd926eff07252bc290e3139f
sei <sei.email@example.com>
2023-02-01 20:25:05 +0000

    11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # 記憶トークンに対応する仮属性を用意する。
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   # コールバック
-  before_save { email.downcase! }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   # ヴァリデーション
   validates :name,  presence: true, length: { maximum:50 }
@@ -54,4 +55,18 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+
+  private
+
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email.downcase!
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token   = User.new_token
+      self.activation_digest  = User.digest(activation_token)
+    end
 end
