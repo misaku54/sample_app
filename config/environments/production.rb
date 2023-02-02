b536febebb40dd5d865c412c22c15d2d70a13574
sei <sei.email@example.com>
2023-02-02 03:06:19 +0000

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  host = 'https://sample-app-mrqu.onrender.com.onrender.com'
+  host = 'https://sample-app-mrqu.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
