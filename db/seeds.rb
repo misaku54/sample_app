3e2bd7d333f34fef3579540439a1853501e94817
sei <sei.email@example.com>
2023-02-05 00:53:33 +0000

    a

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ end
 
 # ユーザーの一部を対象にマイクロポストを生成する
 users = User.order(:created_at).take(6)
-50.times do
+2.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
