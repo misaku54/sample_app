88388eaecf15a6302f3e5f5d418f898d8c414875
sei <sei.email@example.com>
2023-02-06 07:04:40 +0000

    14

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
   # 記憶トークンに対応する仮属性を用意する。
   attr_accessor :remember_token, :activation_token, :reset_token
 
