167e5ba85401b06b984e64308f7a6cc9e0030875
sei <sei.email@example.com>
2023-02-06 08:33:05 +0000

    14.1.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
   # class_nameは探索するモデル名を指定できる。foreign_keyは探索に使う外部キーを指定できる。
   # オプションを設定せず、デフォルトのままだとactive_relationshipモデルのuser_idを検索しようとする。
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
   # 記憶トークンに対応する仮属性を用意する。
   attr_accessor :remember_token, :activation_token, :reset_token
